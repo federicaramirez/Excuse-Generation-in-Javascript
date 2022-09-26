2a44aa277bc7a5036bb754cf79071e2345ebea67
federicaramirez <federicaaramirez@gmail.com>
2022-09-26 18:12:52 +0000

    linkear js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoindex = Math.floor(Math.random(who) * who.length);
+  let whatindex = Math.floor(Math.random(what) * what.length);
+  let whenindex = Math.floor(Math.random(when) * when.length);
+
+  console.log(who[whoindex] + "" + what[whatindex] + "" + when[whenindex]);
+  return (document.getElementById("excuse").innerHTML =
+    who[whoindex] + " " + what[whatindex] + " " + when[whenindex]);
 };
